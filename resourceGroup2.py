88e2e5f11a44a6b7afdcfb153a1019b2c4f69189
Ahsan Naveed <anaveed@dincloud.com>
2022-01-19 23:08:24 +0500

    updated resource group functions

--- resourceGroup2.py
+++ resourceGroup2.py
@@ -47,6 +47,38 @@ class ResourceGroup:
                 kwargs["resource_group"] = self.prompt_with_default("Azure Region", DefaultSettings.region)
         return resource_group.id
 
+    def list_resource_groups(self):
+        # List Resource Groups
+        print("List Resource Groups")
+        for item in self._resource_client.resource_groups.list():
+            self.print_item(item)
+
+
+    def delete_resource_group(self, group_name):
+        print("Delete Resource Group")
+        if group_name in self._resource_client.resource_groups.list():
+            delete_async_operation = self._resource_client.resource_groups.begin_delete(group_name)
+            delete_async_operation.wait()
+        else:
+            print("No resource group found")
+        print("\nDeleted: {}".format(group_name))
+
+    def print_item(self, group):
+        """Print a ResourceGroup instance."""
+        print("\tName: {}".format(group.name))
+        print("\tId: {}".format(group.id))
+        print("\tLocation: {}".format(group.location))
+        print("\tTags: {}".format(group.tags))
+        self.print_properties(group.properties)
+
+    @staticmethod
+    def print_properties(props):
+        """Print a ResourceGroup properties instance."""
+        if props and props.provisioning_state:
+            print("\tProperties:")
+            print("\t\tProvisioning State: {}".format(props.provisioning_state))
+        print("\n\n")
+
     @staticmethod
     def prompt_with_default(key, value):
         user_value = input("{0} [{1}]: ".format(key, value))
